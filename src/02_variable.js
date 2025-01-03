6931cccfb06387a4b102abc990694c9cc70fb52e
Stk8827 <cjng@web.de>
2025-01-03 09:18:41 +0000

    03_verkettung+expressions

--- src/02_variable.js
+++ src/02_variable.js
@@ -1,4 +1,17 @@
 // console.log("Frohes neues!");
 // Zeilenkommentar
+// Blockkomentar 
 
-Blockkommentar
+/***** Variablen 01 *******/
+
+console.log("Hi!");
+console.log("Hi");
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe 
+firstName = "Max"; // Wertzuweisung / assignment
+console.log(firstName); // Ausgabe 
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausdruck / Expression
