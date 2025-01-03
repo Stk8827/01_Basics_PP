ea1d1f1575ce8a24fda83803887cb2decc5958a6
Stk8827 <cjng@web.de>
2025-01-03 10:11:51 +0000

    05_Variablen vs. Konstanten in JS

--- src/02_variable.js
+++ src/02_variable.js
@@ -21,19 +21,32 @@
     JS ist eine untypisierte Sprache!
 */
 
-let test; // Deklaration
-console.log("Datentyp: " + typeof test); // Datentyp ausgeben
-console.log("Inhalt: " + test);
-console.log("----------------------");
-test = "hello"; // String
-console.log("Datentyp: " + typeof test); // Datentyp ausgeben
-console.log("Inhalt: " + test);
-console.log("----------------------");
-test = 2; // Number + Überschreiben
-console.log("Datentyp: " + typeof test); // Datentyp ausgeben
-console.log("Inhalt: " + test);
-console.log("----------------------");
-test = true // Boolsche Algebra / Boolean 
-console.log("Datentyp: " + typeof test); // Datentyp ausgeben
-console.log("Inhalt: " + test);
-console.log("----------------------");
+// let test; // Deklaration
+// console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+// console.log("Inhalt: " + test);
+// console.log("----------------------");
+// test = "hello"; // String
+// console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+// console.log("Inhalt: " + test);
+// console.log("----------------------");
+// test = 2; // Number + Überschreiben
+// console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+// console.log("Inhalt: " + test);
+// console.log("----------------------");
+// test = true // Boolsche Algebra / Boolean 
+// console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+// console.log("Inhalt: " + test);
+// console.log("----------------------");
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test;  // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "Hi!"; // Deklaration + Wertzuweisung gemeinsam!
+test2 = "Hello!"; // Laufzeitfehler
+console.log("Inhalt: " + test2); // Ausgabe
