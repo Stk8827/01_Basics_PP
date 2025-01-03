89b36b63164ea4b2c9f2d6b01d78130a0f821287
Stk8827 <cjng@web.de>
2025-01-03 09:52:08 +0000

    04_ Wertzuweisung II + Datentypen

--- src/02_variable.js
+++ src/02_variable.js
@@ -4,14 +4,36 @@
 
 /***** Variablen 01 *******/
 
-console.log("Hi!");
-console.log("Hi");
+// console.log("Hi!");
+// console.log("Hi");
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe 
-firstName = "Max"; // Wertzuweisung / assignment
-console.log(firstName); // Ausgabe 
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausdruck / Expression
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe 
+// firstName = "Max"; // Wertzuweisung / assignment
+// console.log(firstName); // Ausgabe 
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausdruck / Expression
+
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist eine untypisierte Sprache!
+*/
+
+let test; // Deklaration
+console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+console.log("Inhalt: " + test);
+console.log("----------------------");
+test = "hello"; // String
+console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+console.log("Inhalt: " + test);
+console.log("----------------------");
+test = 2; // Number + Überschreiben
+console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+console.log("Inhalt: " + test);
+console.log("----------------------");
+test = true // Boolsche Algebra / Boolean 
+console.log("Datentyp: " + typeof test); // Datentyp ausgeben
+console.log("Inhalt: " + test);
+console.log("----------------------");
