e159269a4cccda9c9118a4d02922eab673366d6c
Stk8827 <cjng@web.de>
2025-01-08 08:10:00 +0000

    08_Kapselung von Codeblöcken

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,13 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | Call
+test();
+
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo, Manuel!");
+}
